9b9dccacf5619fc3399609e6824500d756aec540
kiko81 <k.kolev@tobo-bg.com>
2015-08-21 23:56:00 +0300

    Added Development Tools homework to HQC course

---
+++ Module 2/01_High Quality Code/15. Development Tools/T4Demo/TextTemplate1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class T4Demo
+{
+    static void Main()
+    {
+        Console.WriteLine("The square of 0 is 0");
+        Console.WriteLine("The square of 1 is 1");
+        Console.WriteLine("The square of 2 is 4");
+        Console.WriteLine("The square of 3 is 9");
+        Console.WriteLine("The square of 4 is 16");
+        Console.WriteLine("The square of 5 is 25");
+        Console.WriteLine("The square of 6 is 36");
+        Console.WriteLine("The square of 7 is 49");
+        Console.WriteLine("The square of 8 is 64");
+        Console.WriteLine("The square of 9 is 81");
+        Console.WriteLine("The square of 10 is 100");
+    }
+
+}
